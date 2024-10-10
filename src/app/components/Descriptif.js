91e5e965c0d89a5ce647320731aae9204f70d93e
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-10-10 14:45:42 +0200

    descriptif component

---
+++ src/app/components/Descriptif.js
@@ -0,0 +1,11 @@
+import styles from "@/app/styles/Descriptif.module.css";
+
+export default function Descriptif() {
+  return (
+    <div className={styles.parent}>
+      <div className={styles.shapeContainer}></div>
+      <div className={styles.photoContainer}></div>
+      <div className={styles.texteContainer}></div>
+    </div>
+  );
+}
