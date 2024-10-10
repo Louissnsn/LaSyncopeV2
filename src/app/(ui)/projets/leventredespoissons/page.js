651e2a7c3706589cb8f481fd5ba5ca90aec6a410
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-10-10 15:03:50 +0200

    add logic

--- src/app/(ui)/projets/leventredespoissons/page.js
+++ src/app/(ui)/projets/leventredespoissons/page.js
@@ -3,11 +3,16 @@ import boue from "@/app/public/images/Covers/boue.jpg";
 import Presse from "@/app/components/Presse";
 import confluence from "@/app/public/images/Spectacles/Poissons/confluence.webp";
 import Distribution from "@/app/components/Distribution";
+import Descriptif from "@/app/components/Descriptif";
+import choeur from "@/app/public/images/Spectacles/Poissons/insectes 3.jpg";
+
 export default function PoissonsPage() {
   const title = ["TOUT CE QU'IL Y A", "DANS LE VENTRE", "DES POISSONS"];
+  const altText = "Photographie du spectacle";
   return (
     <div>
-      <Cover image={boue} altText="Photographie du spectacle" title={title} />
+      <Cover image={boue} altText={altText} title={title} />
+      <Descriptif image={choeur} altText={altText} />
       <Presse image={confluence} altText="Photographie des Poissons" />
       <Distribution />
     </div>
--- src/app/components/Descriptif.js
+++ src/app/components/Descriptif.js
@@ -1,11 +1,19 @@
+"use client";
 import styles from "@/app/styles/Descriptif.module.css";
+import Image from "next/image";
 
-export default function Descriptif() {
+export default function Descriptif({ image, altText, descriptif }) {
   return (
     <div className={styles.parent}>
-      <div className={styles.shapeContainer}></div>
-      <div className={styles.photoContainer}></div>
-      <div className={styles.texteContainer}></div>
+      <div className={styles.shapeContainer}>
+        <div className={styles.shape}></div>
+      </div>
+      <div className={styles.photoContainer}>
+        <Image src={image} alt={altText} />
+      </div>
+      <div className={styles.texteContainer}>
+        <p>{descriptif}</p>
+      </div>
     </div>
   );
 }
