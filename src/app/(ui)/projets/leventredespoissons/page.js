5e9e876aeccb2c803149436a2c96f895357c510f
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-10-10 12:31:56 +0200

    styled components

--- src/app/(ui)/projets/leventredespoissons/page.js
+++ src/app/(ui)/projets/leventredespoissons/page.js
@@ -1,11 +1,15 @@
 import Cover from "@/app/components/Cover";
 import boue from "@/app/public/images/Covers/boue.jpg";
-
+import Presse from "@/app/components/Presse";
+import confluence from "@/app/public/images/Spectacles/Poissons/confluence.webp";
+import Distribution from "@/app/components/Distribution";
 export default function PoissonsPage() {
   const title = ["TOUT CE QU'IL Y A", "DANS LE VENTRE", "DES POISSONS"];
   return (
     <div>
       <Cover image={boue} altText="Photographie du spectacle" title={title} />
+      <Presse image={confluence} altText="Photographie des Poissons" />
+      <Distribution />
     </div>
   );
 }
--- src/app/components/Distribution.js
+++ src/app/components/Distribution.js
@@ -1,3 +1,16 @@
+import styles from "@/app/styles/Distribution.module.css";
+import Image from "next/image";
+import logo from "@/app/public/logo/logo BLANC.png";
 export default function Distribution() {
-  return <div></div>;
+  return (
+    <div className={styles.parent}>
+      <div className={styles.logoContainer}>
+        <Image src={logo} alt="logo de la Syncope" height={600} width={400} />
+      </div>
+      <div className={styles.titreContainer}>
+        <p className={styles.titre}>DISTRIBUTION</p>
+      </div>
+      <div className={styles.texteContainer}></div>
+    </div>
+  );
 }
