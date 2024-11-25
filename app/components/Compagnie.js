403f0b141e463e423282893a02f0ee44b4ecd1e3
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-11-25 10:56:34 +0100

    queries for bigger screens

--- app/components/Compagnie.js
+++ app/components/Compagnie.js
@@ -3,7 +3,7 @@
 import styles from "@/styles/Compagnie.module.css";
 import Image from "next/image";
 import welcome from "../../public/images/images 16:9/poissons.jpg";
-import logo from "../../public/logo/logo BLANC.png";
+import logo from "../../public/logo/logo_BLANC.png";
 
 export default function Compagnie() {
   return (
