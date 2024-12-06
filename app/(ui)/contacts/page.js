287b884053ad056238b59a4a06fb50b9c6964a02
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-12-06 17:22:57 +0100

    create loader component + contact component + animation for loading and link with contacts page

--- app/(ui)/contacts/page.js
+++ app/(ui)/contacts/page.js
@@ -1,8 +1,12 @@
+"use client";
 import styles from "@/styles/Contacts.module.css";
-
+// import Cover from "@/components/Cover";
+// import Contact from "@/components/ContactComponent";
+import Loader from "@/components/Loader";
 export default function Contacts() {
   return (
     <div>
+      <Loader />
       <div className={styles.titre}>
         <p>Un peu de patience, ça arrive</p>
       </div>
---
+++ app/components/Loader.js
@@ -0,0 +1,26 @@
+import styles from "@/styles/Loader.module.css";
+import { useState, useEffect } from "react";
+
+//loader component
+const Preloader = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simule le chargement
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Temps simulé pour le chargement (3s)
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {isLoading && (
+        <div className={styles.preloader}>{/* Div qui recouvre l'écran */}</div>
+      )}
+    </>
+  );
+};
+
+export default Preloader;
