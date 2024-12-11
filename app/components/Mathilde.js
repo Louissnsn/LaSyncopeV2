594353a908eb9721accecd0ae0718ee952f11c0a
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-12-11 11:14:01 +0100

    change Mathilde's bio

--- app/components/Mathilde.js
+++ app/components/Mathilde.js
@@ -32,35 +32,38 @@ export default function Mathilde() {
           Après une première formation en tant que comédienne au Conservatoire
           d&apos;Art Dramatique de Toulouse, Mathilde Bellin intègre le
           département d&apos;études théâtrales de l&apos;École Normale
-          Supérieure de Lyon en 2013.
-        </p>
-        <p className={styles.texteBio}>
-          Elle y expérimente durant deux ans l&apos;écriture dramatique auprès
-          de Joris Mathieu, Jean-Loup Rivière et Samuel Gallet. En 2016, elle
-          intègre le TNP de Villeurbanne en qualité de dramaturge et conseillère
-          littéraire auprès de Christian Schiaretti, qu&apos;elle va accompagner
-          durant un an.
+          Supérieure de Lyon en 2013. Elle y expérimente durant deux ans
+          l&apos;écriture dramatique auprès de Joris Mathieu, Jean-Loup Rivière
+          et Samuel Gallet. En 2016, elle intègre le TNP de Villeurbanne en
+          qualité de dramaturge et conseillère littéraire auprès de Christian
+          Schiaretti, qu&apos;elle va accompagner durant un an.
         </p>
         <p className={styles.texteBio}>
           En 2017, elle rejoint le Laboratoire de Formation au Théâtre Physique,
           où elle achève sa formation de comédienne. De 2018 à 2021, Mathilde
           Bellin est comédienne au sein de différentes compagnies, et doctorante
           en Études Théâtrales à l’Université Paris 8, où elle enseigne
-          l&apos;Histoire de la mise en scène.
+          notamment l&apos;Histoire de la mise en scène.
         </p>
         <p className={styles.texteBio}>
-          En 2019, elle co-fonde la Syncope, avec qui elle écrit et monte son
-          premier spectacle,{" "}
+          Co-fondatrice et co-directrice artistique de la compagnie de théâtre «
+          La Syncope», elle écrit et met en scène{" "}
           <em>Tout ce qu&apos;il y a dans le ventre des poissons</em> (Prix du
-          Jury du Festival de Théâtre de Maisons-Laffitte, 2022).
+          Jury du Festival de Théâtre de Maisons-Laffitte en 2022, Prix des
+          jeunes auteur·ice·s de théâtre du Festival Bioviv&apos;art) ainsi
+          qu&apos;un second spectacle à destination des jeunes publics, intitulé
+          <em> Quand le loup n&apos;y est plus </em>, en tournée depuis 2022.
         </p>
         <p className={styles.texteBio}>
-          Depuis cette date, elle donne également des cours de théâtre amateurs
-          et des ateliers d&apos;écriture dans différentes structures publiques
-          (médiathèques, associations). En 2024, elle est enseignante en Arts du
-          spectacle à l&apos;Université Lyon 2 et à la mise en scène de
-          l&apos;opéra <em>Lakmé</em>.
+          Depuis septembre 2023, elle est enseignante vacataire en licence
+          d&apos;Arts du spectacle à l&apos;Université Lumière Lyon 2, et donne
+          également des cours de théâtre amateur et des ateliers
+          d&apos;expression corporelle et d’écriture dans différentes structures
+          publiques (médiathèques, associations). En 2024, elle est à la mise en
+          scène de l&apos;opéra <em>Lakmé </em>
+          (création 2025, Théâtre d&apos;Albi).
         </p>
+
         <div className={styles.zigZag}></div>
       </div>
       <div className={styles.barreLatérale}> </div>
