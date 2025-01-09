6cc99a41c186ead871a98a2392745f66b6f897c4
Louissnsn <sanson.louis@gmail.com>
2025-01-09 18:39:35 +0100

    added navigation in gallery component

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -1,26 +1,33 @@
 import styles from "@/styles/Gallery.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const Gallery = ({ images }) => {
+  const getLinkPath = (imageUrl) => {
+    if (imageUrl.includes("Poisson")) return "/projets/leventredespoissons";
+    if (imageUrl.includes("Nobel")) return "/projets/prixnobeldelamour";
+    if (imageUrl.includes("Loup")) return "/projets/quandleloup";
+    if (imageUrl.includes("Ecole")) return "/projets/ecoledete";
+    return "/projets";
+  };
+
   return (
     <div className={styles.gallery}>
-      {images.map((index) => {
-        // console.log(index)
-        return (
-          <div key={index} className={styles.galleryItem}>
+      {images.map((imageUrl, index) => (
+        <div key={index} className={styles.galleryItem}>
+          <Link href={getLinkPath(imageUrl)}>
             <Image
-              // placeholder="blur"
-              src={index}
+              src={imageUrl}
               fill
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               style={{
                 objectFit: "cover",
               }}
-              alt="Photo du spectacle"
+              alt="Photographie du spectacle"
             />
-          </div>
-        );
-      })}
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
