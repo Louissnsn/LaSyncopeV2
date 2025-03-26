097b26266511df99e90c3998b7d6aaa85e6f7973
Louissnsn <sanson.louis@gmail.com>
2025-03-26 18:06:08 +0100

    cleaning

--- app/components/CquoiLaSyncope.js
+++ app/components/CquoiLaSyncope.js
@@ -5,21 +5,20 @@ export default function Cquoi() {
       <h2 className={styles.titre}>LA SYNCOPE</h2>
       <div className={styles.leftContainer}>
         <h3 className={styles.sousTitre}>
-          QU&apos;EST-CE QUE C&apos;EST QUE CETTE SYNCOPE ?
+          QU&apos;EST-CE QUE C&apos;EST QUE CETTE <br />
+          SYNCOPE ?
         </h3>
         <div className={styles.textLeft}>
           <p className={styles.textDescription}>
             Une aventure artistique menée par Mathilde Bellin et Pauline
             Legoëdec, autour desquelles gravitent une multiplicité d’artistes.
           </p>
-          <br />
           <p className={styles.textDescription}>
             La Syncope est une compagnie de théâtre francilienne créée en 2019
             par Pauline Legoëdec et Mathilde Bellin, deux artistes de théâtre
             ayant développé une affinité artistique singulière lors de leurs
             différentes formations.
           </p>
-          <br />
           <p className={styles.textDescription}>
             Elle affirme une grande confiance en l&apos;interprète et mène à
             bien des spectacles qui ont pour terrain de jeu des écritures
--- app/page.js
+++ app/page.js
@@ -1,17 +1,12 @@
-// "use client";
 import styles from "./page.module.css";
 import Cquoi from "./components/CquoiLaSyncope";
 import Collaborateurices from "./components/Collaborateurices";
 import Accueil from "./components/Accueil";
-import Compagnie from "./components/Compagnie";
-// import Loader from "@/components/Loader";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      {/* <Compagnie /> */}
       <Accueil />
-      {/* <Loader /> */}
       <Cquoi />
       <Collaborateurices />
     </div>
