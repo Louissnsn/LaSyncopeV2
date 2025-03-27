0afb6f696c73ede359f713165cc9f7e03328d3b8
Louissnsn <sanson.louis@gmail.com>
2025-03-27 09:42:49 +0100

    editing style in Accueil, caroussel, scrolldown

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import styles from "./page.module.css";
 import Cquoi from "./components/CquoiLaSyncope";
 import Collaborateurices from "./components/Collaborateurices";
 import Accueil from "./components/Accueil";
+// import ScrollDown from "./components/ScrollDown";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Accueil />
       <Cquoi />
+      {/* <ScrollDown /> */}
       <Collaborateurices />
     </div>
   );
